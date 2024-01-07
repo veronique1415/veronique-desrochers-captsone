8b615f1af349a9d5803f5a0c7b714feacb6e75bd
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-07 16:16:01 -0500

    make route for each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
+import HomePage from './pages/HomePage/HomePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import FindUsPage from './pages/FindUsPage/FindUsPage';
+import ListingPage from "./pages/ListingPage/ListingPage";
+import ProducersPage from "./pages/ProducersPage/ProducersPage"
+import ProducerDetailsPage from './pages/ProducerDetailsPage/ProducerDetailsPage';
+import WinesPage from './pages/WinesPage/WinesPage';
+import WineDetailsPage from './pages/WineDetailsPage/WineDetailsPage';
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/About' element={<AboutPage />} />
+        <Route path='/FindUs' element={<FindUsPage />} />
+        <Route path='/Listing' element={<ListingPage />} />
+        <Route path='/Producers' element={<ProducersPage />} />
+        <Route path='/Producers/:producerId' element={<ProducerDetailsPage />} />
+        <Route path='/Wines' element={<WinesPage />} />
+        <Route path='/Wines/:wineId' element={<WineDetailsPage />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
