ffb01f9d71b577642b089727f14f55dc76fecc1c
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-13 14:53:19 -0500

    getting a hold of bootstrap finally

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,46 @@
-import Nav from "../Nav/Nav";
+import searchIcon from "../../assets/icons/search-icon.png"
+import { Link } from "react-router-dom"
+import { DropdownItem, NavDropdown, Container, Row, Col} from "react-bootstrap"
 import "./Header.scss"
-import { Row, Col, Container } from "react-bootstrap";
+
 
 const Header = () => {
     return (
         <header className="header">
-            <Container>
-                <Row>
-                    <Col className="header__container" >
-                    <h2 className="header__heading">Grand Ordinaire.</h2>
+            <nav>
+            <Container style={{ marginRight: 0}} className="mw-100">
+                <Row className="pr-0">
+                    <Col className="header__container py-3" xs={8} md={10}>
+                        <h2 className="header__heading">Grand Ordinaire.</h2>
                     </Col>
-                    <Col>
-                    <Nav/>
+                    <Col xs={2} md={1} className="col__search">
+                        <button className="search__button search__buton--hidden"><img className="search__icon  search__icon--hidden" src={searchIcon} alt="search icon"></img></button>
                     </Col>
-                    
-                </Row>
-            
-            </Container>
+                    <Col xs={2} md={1}className="col__menu py-3">
+                        
+                        <NavDropdown className="nav__dropdown">
+                            <ul>
+                                <li><DropdownItem>
+                                    <Link to={"/about"}>About</Link>
+                                </DropdownItem></li>
+                                <li><DropdownItem>
+                                    <Link to={"/producers"}>Producers</Link>
+                                </DropdownItem></li>
+                                <li><DropdownItem>
+                                    <Link to={"/wines"}>Wines</Link>
+                                </DropdownItem></li>
+                                <li><DropdownItem>
+                                    <Link to={"/findUs"}>Find us</Link>
+                                </DropdownItem></li>
+                                <li><DropdownItem>
+                                    <Link to={"/listing"}>Listing</Link>
+                                </DropdownItem></li>
+                                </ul>
+                            </NavDropdown>
+                        </Col>
+                    </Row>
+                </Container>
+            </nav>
         </header>
     )
 }
