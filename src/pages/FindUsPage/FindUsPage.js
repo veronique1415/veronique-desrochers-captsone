fae324e8645c7e43e7e827cd895427940609cc88
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-16 18:24:34 -0500

    add .env file and start messing with google maps api

--- src/pages/FindUsPage/FindUsPage.js
+++ src/pages/FindUsPage/FindUsPage.js
@@ -1,6 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
+
 const FindUsPage = () => {
+const position = {lat: 45.53303527832031, lng: -73.6106185913086}
+
     return (
-        <div>FindUsPage</div>
+        <APIProvider apiKey={process.env.GOOGLE_MAPS_KEY}>
+            <div>
+                <Map zoom={9} center={position}></Map>
+            </div>
+        </APIProvider>
+        
     )
 }
 
