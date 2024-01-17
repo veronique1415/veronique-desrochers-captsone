3048093791e30f57e84c68a1a5c785a9c0633474
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-17 10:04:15 -0500

    make container around map and chanmge position so it's montreal

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -8,31 +8,30 @@ import {
   Pin,
   InfoWindow,
 } from "@vis.gl/react-google-maps";
+import "./Map.scss"
 
 const MapComp = () => {
-  const position = { lat: 53.54, lng: 10 };
+  const position = { lat: 45.508888, lng: -73.561668 };
   const [open, setOpen] = useState(false);
 
   return (
-    <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
-      <div style={{ height: "100vh", width: "100%" }}>
-        <Map zoom={9} center={position} mapId={process.env.REACT_APP_GOOGLE_MAPS_STYLE}>
-          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-            <Pin
-              background={"grey"}
-              borderColor={"green"}
-              glyphColor={"purple"}
-            />
-          </AdvancedMarker>
+    <article className="map__container">
+      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
+        <div className="map">
+          <Map zoom={13} center={position} mapId={process.env.REACT_APP_GOOGLE_MAPS_STYLE}>
+            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+              <Pin />
+            </AdvancedMarker>
 
-          {open && (
-            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-              <p>I'm in Hamburg</p>
-            </InfoWindow>
-          )}
-        </Map>
-      </div>
-    </APIProvider>
+            {open && (
+              <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                <p>I'm in Hamburg</p>
+             </InfoWindow>
+            )}
+          </Map>
+        </div>
+      </APIProvider>
+    </article>  
   );
 }
 
