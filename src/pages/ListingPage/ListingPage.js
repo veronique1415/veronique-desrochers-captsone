681daf869b2e9777ce3965f8c6aa117c0ff495dc
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-24 15:45:06 -0500

    add a button to download pdf as well

--- src/pages/ListingPage/ListingPage.js
+++ src/pages/ListingPage/ListingPage.js
@@ -6,9 +6,16 @@ const Listing = () => {
 
     return (
         <main>
-            <div className="pdf__container">
-                <iframe src={process.env.PUBLIC_URL + '/Grand-Ordinaire-Jan2024.pdf'} title="grand-ordinaire-pdf"  className="pdf"></iframe>
-            </div>
+            <section className="pdf__section">
+                <div className="pdf__container">
+                    <iframe src={process.env.PUBLIC_URL + '/Grand-Ordinaire-Jan2024.pdf'} title="grand-ordinaire-pdf"  className="pdf"></iframe>
+                </div>
+                <div className="pdf__button--container">
+                    <a href={process.env.PUBLIC_URL + '/Grand-Ordinaire-Jan2024.pdf'} download={"grand-ordinaire-listing.pdf"} className="pdf__button--link">
+                        <button type="download" className="pdf__button">Click to download</button>
+                    </a>
+                </div>
+            </section>
         </main>
     )
 }
