b4c1c0359ab6c00bd8b21fd271c489482175b75c
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-24 16:21:47 -0500

    make map function to iterate over every bottle to get its image, id and name for cards

--- src/components/NewIn/NewIn.js
+++ src/components/NewIn/NewIn.js
@@ -1,10 +1,10 @@
 import {Container, Row, Col, Card} from "react-bootstrap"
 import { Link } from "react-router-dom"
-import Bottle1 from "../../assets/images/CadeNociO.jpg"
-import Bottle2 from "../../assets/images/CadeNociO2.jpg"
-import Bottle3 from "../../assets/images/VenierR.jpg"
-import Bottle4 from "../../assets/images/VenierR3.jpg"
-import Bottle5 from "../../assets/images/CadeNociR3.jpg"
+// import Bottle1 from "../../assets/images/CadeNociO.jpg"
+// import Bottle2 from "../../assets/images/CadeNociO2.jpg"
+// import Bottle3 from "../../assets/images/VenierR.jpg"
+// import Bottle4 from "../../assets/images/VenierR3.jpg"
+// import Bottle5 from "../../assets/images/CadeNociR3.jpg"
 import Banner from "../Banner/Banner";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -31,69 +31,28 @@ const NewIn = () => {
     }
     console.log("bottle",bottles)
 
-    const {product_image, product_name} = bottles[0]
+    const {product_image, product_name, product_id} = bottles[0]
 
   return <section className="newIn">
             <Banner text={"New arrivals"} />
             <Container fluid className="py-2 pb-2">
                 <Row xs={2} sm={3} md={4} lg={5} className="image__flex-container">
-                    <Col className="py-1">
-                        <Link to={"/wines"}><Card>
-                            <Card.Img src={product_image}/>
+                    {bottles.map((bottle) => (
+                            <Col className="py-1">
+                        <Link to={`/wines/${product_id}`}>
+                            <Card key={product_id}>
+                        <Card.Img src={product_image}/>
                             <Card.ImgOverlay className="card__overview">
-                                <h3>Ca de Noci</h3>
-                                <p>Amazing product</p>
+                                <h3>{product_name}</h3>
                             </Card.ImgOverlay>
                         </Card></Link>
-                    </Col>
-                    <Col className="py-1">
-                        <Link to={"/wines"}><Card>
-                            <Card.Img src={Bottle1}/>
-                            <Card.ImgOverlay className="card__overview">
-                                <h3>Ca de Noci</h3>
-                                <p>Amazing product</p>
-                            </Card.ImgOverlay>
-                    </Card></Link>
-                    </Col>
-                    <Col className="py-1">
-                        <Link to={"/wines"}><Card>
-                            <Card.Img src={Bottle2}/>
-                            <Card.ImgOverlay className="card__overview">
-                                <h3>Ca de Noci</h3>
-                                <p>Amazing product</p>
-                            </Card.ImgOverlay>
-                    </Card></Link>
-                    </Col>
-                    <Col className="py-1">
-                        <Link to={"/wines"}><Card>
-                            <Card.Img src={Bottle3}/>
-                            <Card.ImgOverlay className="card__overview">
-                            <h3>Ca de Noci</h3>
-                            <p>Amazing product</p>
-                        </Card.ImgOverlay>
-                    </Card></Link>
-                    </Col>
-                    <Col className="py-1">
-                    <Link to={"/wines"}><Card>
-                       <Card.Img src={Bottle4}/>
-                        <Card.ImgOverlay className="card__overview">
-                            <h3>Ca de Noci</h3>
-                            <p>Amazing product</p>
-                        </Card.ImgOverlay>
-                    </Card></Link>
-                    </Col>
-                    <Col className="py-1">
-                    <Link to={"/wines"}><Card>
-                       <Card.Img src={Bottle5}/>
-                        <Card.ImgOverlay className="card__overview">
-                            <h3>Ca de Noci</h3>
-                            <p>Amazing product</p>
-                        </Card.ImgOverlay>
-                    </Card></Link>
-                    </Col>
+                        </Col>
+                        ))}
                 </Row>
             </Container>
         </section>
 };
 
 export default NewIn;
+
+
