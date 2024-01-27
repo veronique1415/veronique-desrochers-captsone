81ae82ead614494e1b8b70f07f4f26beaa972e7d
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-27 17:43:39 -0500

    make the footer a bit better

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,6 +15,8 @@ const Footer = () => {
                     <ul className="footer__list">
                         <li className="footer__item"><Link to={"/about"} className="footer__link">About</Link></li>
                         <li className="footer__item"><Link to={"/producers"} className="footer__link">Producers</Link></li>
+                    </ul>
+                    <ul className="footer__list">
                         <li className="footer__item"><Link to={"/wines"} className="footer__link">Available wines</Link></li>
                         <li className="footer__item"><Link to={"/findUs"} className="footer__link">Find us!</Link></li>
                     </ul>
@@ -22,7 +24,8 @@ const Footer = () => {
                 <Col className="footer__containers">
                 <h2>LOGO</h2>
                 </Col>
-                <Col className="footer__containers">
+                <Col className="footer__containers justify-content-center">
+                <p className="footer__contact-us">Contact us</p>
                 <button className="footer__email" onClick={() => window.location.href = 'mailto:larry@grandordinaire.com'}><FontAwesomeIcon icon={faEnvelope} className='fa-2xl'/></button>
                 <Link to={"https://www.instagram.com/grand.ordinaire/"} className="sidenav__link sidenav__logo"><FontAwesomeIcon icon={faInstagram} className='fa-2xl'/></Link>
                 </Col>
