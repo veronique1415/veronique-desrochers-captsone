2712ab63630cadf3ec08c4f529bde0ffd299d5dc
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-29 18:13:51 -0500

    testing height and width for new in section's list

--- src/components/NewIn/NewIn.js
+++ src/components/NewIn/NewIn.js
@@ -35,19 +35,21 @@ const NewIn = () => {
 
   return <section className="newIn">
             <Banner text={"New arrivals"} />
-            <ul class="list tablet--new">
-                {bottles.map((bottle) => (
-                <>  
-                <li key={bottle.product_id} className="list__item"><Link className="list__link" to={`/wines/${bottle.product_id}`}>
-                    <img className="list__img" src={bottle.product_image} alt={bottle.product_name}></img>
-                    <div className="list__item--overview">
-                    <h3 className="list__item--text">{bottle.product_name}</h3>
-                    </div>
-                </Link>
-                </li>
-                </>  
-            ))}
-            </ul>
+            <section>
+                <ul class="list tablet--new">
+                    {bottles.map((bottle) => (
+                    <>  
+                    <li key={bottle.product_id} className="list__item"><Link className="list__link" to={`/wines/${bottle.product_id}`}>
+                        <img className="list__img" src={bottle.product_image} alt={bottle.product_name}></img>
+                        <div className="list__item--overview">
+                        <h3 className="list__item--text">{bottle.product_name}</h3>
+                        </div>
+                    </Link>
+                    </li>
+                    </>  
+                ))}
+                </ul>
+            </section>
             <Container fluid className="pb-4 mobile--new">
                 <Row xs={2} sm={3} md={4} lg={6} className="product__card--row">
                     {bottles.map((bottle) => (
