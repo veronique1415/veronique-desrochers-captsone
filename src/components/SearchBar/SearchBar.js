41a5092e6ffd47fcecbc53c4ec36d4a956ef1293
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-02-01 17:41:40 -0500

    add a div that displays either producers or products

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Form } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "./SearchBar.scss";
 
@@ -9,6 +10,8 @@ const searchProductsUrl = `${baseUrl}products/search?s=`;
 
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [producerResults, setProducerResults] = useState([]);
+  const [productsResults, setProductsResults] = useState([]);
 
   const handleSearchInputChange = (event) => {
     setSearchInput(event.target.value);
@@ -21,12 +24,12 @@ const SearchBar = () => {
           const producerResponse = await axios.get(
             `${searchProducerUrl}${searchInput}`
           );
-          console.log("producer response", producerResponse.data);
+          setProducerResults(producerResponse.data);
 
           const productsResponse = await axios.get(
             `${searchProductsUrl}${searchInput}`
           );
-          console.log("products response", productsResponse.data);
+          setProductsResults(productsResponse.data);
         }
       } catch (error) {
         console.error("Error fetching data", error);
@@ -58,6 +61,37 @@ const SearchBar = () => {
           </Col>
         </Row>
       </Container>
+
+     {producerResults.length > 0 ?  
+      (
+     <Container>
+        <Row>
+          <Col>
+            <h2>Producer Results</h2>
+            {producerResults.map((producer) => (
+              <div key={producer.producer_id}>
+                <Link className="search__link" to={`producers/${producer.producer_id}`}><p>{producer.producer_name}</p></Link>
+                {/* Add more details if needed */}
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container> ) :  <p>No Matching producers</p>}
+      
+      {productsResults.length > 0 ? (
+        <Container>
+        <Row>
+          <Col>
+            <h2>Products Results</h2>
+            {productsResults.map((product) => (
+              <div key={product.product_id}>
+                <Link to={`/wines/${product.product_id}`}><p>{product.product_name}</p></Link>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+      ) : <p>No matching product</p>}
     </section>
   );
 };
