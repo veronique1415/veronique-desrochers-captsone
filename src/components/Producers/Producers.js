ab7224554abe731f21907049230c33d364e84014
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-02-02 09:30:53 -0500

    refactor producers to do axios call on producersPage instead and pass props to producers component

--- src/components/Producers/Producers.js
+++ src/components/Producers/Producers.js
@@ -1,53 +1,32 @@
-
-import { Container, Row, Card, Col} from "react-bootstrap";
+import { Container, Row, Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Banner from "../Banner/Banner";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import "./Producers.scss"
+import "./Producers.scss";
 
-const Producers = ({text}) => {
-    const baseUrl = process.env.REACT_APP_BASE_URL
-    const producersUrl = `${baseUrl}producers`
-    const [producers, setProducers] = useState([]);
-
-    const getProducers = async () => {
-        try {
-            const response = await axios.get(producersUrl)
-            setProducers(response.data)
-        } catch (error) {
-            console.error("Unable to retrieve picture", error)
-        }
-    }
-    useEffect(()=>{
-        getProducers()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    if(producers === undefined) {
-        return <div>Loading...</div>
-    }
+const Producers = ({ text, producersData }) => {
+  if (!producersData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="producers">
-        <Banner text={text}/>
-        <Container className="py-3 producers__container--flex" >
-            <Row xs={1} md={3} className="producers__row">
-            {producers.map((producer) => (
-                           <Col className="producers__containers">
-                           <Link to={`/producers/${producer.producer_id}`} className="producers__link">
-                                   <Card key={producer.producer_id}>
-                                   <Card.Img src={producer.producer_image} alt={producer.producer_name}/>
-                                   </Card>
-                                   <h3 className="producers__name">{producer.producer_name}</h3>
-                               </Link>
-                           </Col>
-                        ))}
-            </Row>
-        </Container>
+      <Banner text={text} />
+      <Container className="py-3 producers__container--flex">
+        <Row xs={1} md={3} className="producers__row">
+          {producersData.map((producer) => (
+            <Col className="producers__containers" key={producer.producer_id}>
+              <Link to={`/producers/${producer.producer_id}`} className="producers__link">
+                <Card>
+                  <Card.Img src={producer.producer_image} alt={producer.producer_name} />
+                </Card>
+                <h3 className="producers__name">{producer.producer_name}</h3>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </section>
-
-  )
+  );
 };
 
 export default Producers;
--- src/pages/ProducersPage/ProducersPage.js
+++ src/pages/ProducersPage/ProducersPage.js
@@ -1,13 +1,32 @@
-
 import Producers from "../../components/Producers/Producers";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProducersPage = () => {
-    return (
-        <main>
-            <Producers text={"Our Producers"} />
-        </main>
-        
-    )
-}
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  const producersUrl = `${baseUrl}producers`;
+
+  const [producers, setProducers] = useState([]);
+
+  useEffect(() => {
+    const getProducers = async () => {
+      try {
+        const response = await axios.get(producersUrl);
+        setProducers(response.data);
+      } catch (error) {
+        console.error("Unable to retrieve producers", error);
+      }
+    };
+
+    getProducers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <main>
+      <Producers text={"Our Producers"} producersData={producers} />
+    </main>
+  );
+};
 
 export default ProducersPage;
