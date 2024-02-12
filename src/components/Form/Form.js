f280c61c34652987befe75c7902f20391a20649d
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-02-12 16:19:27 -0500

    add form for sign in admin basic

---
+++ src/components/Form/Form.js
@@ -0,0 +1,33 @@
+import { Form, FormGroup, FormControl, Col, ControlLabel, Button } from "react-bootstrap";
+import "./Form.scss";
+
+const FormComp = () => {
+  return (
+    <Form horizontal>
+  <FormGroup controlId="formHorizontalEmail">
+    <Col componentClass={ControlLabel} sm={2}>
+      Email
+    </Col>
+    <Col sm={10}>
+      <FormControl type="email" placeholder="Email" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup controlId="formHorizontalPassword">
+    <Col componentClass={ControlLabel} sm={2}>
+      Password
+    </Col>
+    <Col sm={10}>
+      <FormControl type="password" placeholder="Password" />
+    </Col>
+  </FormGroup>
+  <FormGroup>
+    <Col smOffset={2} sm={10}>
+      <Button type="submit">Sign in</Button>
+    </Col>
+  </FormGroup>
+</Form>
+  )
+};
+
+export default FormComp;
