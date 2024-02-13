b7e22d0cf481b5376161982033cbab1f4c4e3795
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-02-13 10:37:01 -0500

    add functionnality to log in specific user

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,25 +1,47 @@
 import Button from "../Button/Button";
 import { Form, FormGroup, FormControl, Col, FormLabel } from "react-bootstrap";
 import "./Form.scss";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const FormComp = () => {
+  const [error, setError] = useState("")
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("url", {
+        email: event.target.email.value,
+        password: event.target.password.value
+      })
+      console.log(response.data);
+      sessionStorage.setItem("token", response.data.token)
+      navigate("/dashboard")
+
+    } catch(error) {
+      setError("Something went wrong")
+    }
+  }
   return (
-    <Form horizontal className="py-2 form__login">
+    <Form horizontal className="py-2 form__login" onSubmit={handleSubmit}>
   <FormGroup className="form__login--group">
-    <FormLabel className="form__login--label">
+    <FormLabel className="form__login--label" htmlFor="#email">
       Email
       <Col>
-      <FormControl type="email" placeholder="Email" className="form__login--input"/>
+      <FormControl type="email" placeholder="Email" value={} id="email" name="email" className="form__login--input"/>
       <FormControl.Feedback />
     </Col>
     </FormLabel>
   </FormGroup>
 
   <FormGroup className="form__login--group">
-    <FormLabel className="form__login--label">
+    <FormLabel className="form__login--label" htmlFor="#password">
       Password
       <Col>
-      <FormControl type="password" placeholder="Password" className="form__login--input"/>
+      <FormControl type="password" placeholder="Password" value={} id="password" name="password" className="form__login--input"/>
       <FormControl.Feedback />
     </Col>
     </FormLabel>
